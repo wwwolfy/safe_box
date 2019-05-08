b54f6f28725c46ad4afa2132dbf91a67a191657e
Vukovojci <webwule@gmail.com>
2019-05-09 00:19:18 +0200

    Updates

---
+++ precache-manifest.6eab585976c61a4b0dce8b35b63143d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711306f4bce9ed66d7e4cece2171246c",
+    "url": "/safe_box/index.html"
+  },
+  {
+    "revision": "15fb21a3fcb428c079e7",
+    "url": "/safe_box/static/css/main.1623327d.chunk.css"
+  },
+  {
+    "revision": "979821f6756ba7dc378a",
+    "url": "/safe_box/static/js/2.7569e5d4.chunk.js"
+  },
+  {
+    "revision": "15fb21a3fcb428c079e7",
+    "url": "/safe_box/static/js/main.01f0a9d2.chunk.js"
+  },
+  {
+    "revision": "b3e8c53e02fcaf9d3800",
+    "url": "/safe_box/static/js/runtime~main.c1bac5f7.js"
+  }
+]);
